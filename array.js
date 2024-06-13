2823add44731eb4bb62cae86e034b367e7c2d447
Praveen Raj K <praveenraj.krishnamoorthy@outlook.com>
2024-06-13 21:14:22 +0530

    learning

---
+++ array.js
@@ -0,0 +1,43 @@
+// let arr = [undefined,"second","third","fourth"];
+
+// --
+// let x,y,z
+// [x,y,z]=arr
+
+// [x,,z] = arr
+// console.log(z);
+
+// [x,...y] = arr
+// console.log(y[1]);
+
+// when destructuring we cannot reassing a value until it is empty or set to undefined
+// let arr = [undefined,"second","third","fourth"];
+// let arr = [undefined,"second","third","fourth"];
+// [x = "newValue",y,z] = arr
+// console.log(x,y,z);
+
+
+// --
+
+
+// swap assign method
+// let a = 10
+// let b = 20
+// c = a
+// // c = 10
+// a = b
+// // a = 20
+// b = c
+// // b = 10
+// console.log("a: " + a);
+// console.log("b: " + b);
+
+// swap array method
+// let a = 10;
+// let b = 20;
+
+// // [b, a] = [a, b]
+// // [a, b] = [b, a]
+// console.log(a);
+// console.log(b);
+
